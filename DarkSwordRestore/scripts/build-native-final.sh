a33a71613eb34cd74ab7db6dfa075bf36d1bf518
Zyn <bobbytatum999@gmail.com>
2026-07-21 01:59:47 -0500

    fix(darksword): define Apple unused annotation for MinGW

--- DarkSwordRestore/scripts/build-native-final.sh
+++ DarkSwordRestore/scripts/build-native-final.sh
@@ -18,6 +18,7 @@ set -x
 export PATH="/mingw64/bin:/usr/bin:$PATH"
 export PKG_CONFIG_PATH="/mingw64/lib/pkgconfig:/mingw64/share/pkgconfig"
 export MAKEFLAGS="-j2"
+export CPPFLAGS="${CPPFLAGS:-} -D__unused="
 
 build_autotools() {
   local repository="$1"
